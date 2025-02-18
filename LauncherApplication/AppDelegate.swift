c6eadad8c90661fd2c73c838e790273bd81cb1c6
isaacphilipose <isaacvp2@gmail.com>
2025-02-17 21:02:57 -0500

    added feature to make app start in background on login instead of foreground

--- LauncherApplication/AppDelegate.swift
+++ LauncherApplication/AppDelegate.swift
@@ -22,27 +22,35 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let isRunning = !runningApps.filter { $0.bundleIdentifier == mainAppIdentifier }.isEmpty
         
         if !isRunning {
-            DistributedNotificationCenter.default().addObserver(self,
-                                                                selector: #selector(self.terminate),
-                                                                name: Notification.Name("killLauncher"),
-                                                                object: mainAppIdentifier)
+            DistributedNotificationCenter.default().addObserver(
+                self,
+                selector: #selector(self.terminate),
+                name: Notification.Name("killLauncher"),
+                object: mainAppIdentifier
+            )
             
             let path = Bundle.main.bundlePath as NSString
             var components = path.pathComponents
             components.removeLast(3)
             components.append("MacOS")
-            let appName = "Hidden Bar"
-            components.append(appName) //main app name
+            let appName = "Hidden Bar" // main app name
+            components.append(appName)
             let newPath = NSString.path(withComponents: components)
-            NSWorkspace.shared.launchApplication(newPath)
-        }
-        else {
+            let newPathURL = URL(fileURLWithPath: newPath)
+            
+            // Use NSWorkspace.OpenConfiguration to hide and not activate the app
+            let configuration = NSWorkspace.OpenConfiguration()
+            configuration.hides = true
+            configuration.activates = false
+            
+            do {
+                try NSWorkspace.shared.openApplication(at: newPathURL, configuration: configuration)
+            } catch {
+                print("Error launching app: \(error)")
+            }
+        } else {
             self.terminate()
         }
-    }
-    
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
     
     
